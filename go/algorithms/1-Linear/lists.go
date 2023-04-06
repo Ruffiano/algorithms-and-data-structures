338372231e4d665952ddb4ba968b782fd817d37e
ruffiano <ruffianodev@gmail.com>
2023-04-06 21:30:45 +0900

    list

--- go/algorithms/1-Linear/lists.go
+++ go/algorithms/1-Linear/lists.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"container/list"
+	"fmt"
 )
 
 func main() {
 	var intList list.List
-	intList.Push
+	intList.PushBack(11)
+	intList.PushBack(23)
+	intList.PushBack(34)
+
+	for element := intList.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int))
+	}
 }
